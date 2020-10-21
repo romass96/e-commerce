7e83d8d29d1dd132a7e548b16c056e3652775453
Roman Babii <romababiy96@gmail.com>
2020-10-21 16:31:52 +0300

    fixed search by string for Orders

--- src/main/java/ua/ugolek/repository/ExtendedOrderRepository.java
+++ src/main/java/ua/ugolek/repository/ExtendedOrderRepository.java
@@ -22,11 +22,15 @@ import java.util.function.Function;
 @Repository
 public class ExtendedOrderRepository {
 
+    // Names of entity's fields. Should be the same as names of Java class fields
     private static final String TOTAL_ORDER_PRICE_FIELD = "totalOrderPrice";
     private static final String COMMENT_FIELD = "comment";
     private static final String STATUS_FIELD = "status";
     private static final String CREATED_DATE_FIELD = "createdDate";
     private static final String ID_FIELD = "id";
+    private static final String PRODUCT_NAME_FIELD = "name";
+    private static final String ORDER_ITEMS_FIELD = "orderItems";
+    private static final String PRODUCT_FIELD = "product";
 
     @PersistenceContext
     private EntityManager entityManager;
@@ -70,23 +74,25 @@ public class ExtendedOrderRepository {
 
     private <T> void populateQuery(OrderFilter orderFilter, CriteriaQuery<T> query, Root<Order> root) {
         List<Predicate> wherePredicates = new ArrayList<>();
+        List<Predicate> stringForSearchPredicates = new ArrayList<>();
 
         Optional<String> stringForSearchOptional = orderFilter.getStringForSearchOptional();
 
         orderFilter.getCategoryIdOptional().ifPresent(categoryId -> {
-            Join<Order, OrderItem> orderItems = root.join("orderItems");
-            Join<OrderItem, Product> products = orderItems.join("product");
+            Join<Order, OrderItem> orderItems = root.join(ORDER_ITEMS_FIELD);
+            Join<OrderItem, Product> products = orderItems.join(PRODUCT_FIELD);
             wherePredicates.add(criteriaBuilder.equal(products.get(ID_FIELD), categoryId));
 
             stringForSearchOptional.ifPresent(stringForSearch ->
-                    wherePredicates.add(criteriaBuilder.like(products.get("name"), like(stringForSearch))));
+                stringForSearchPredicates.add(
+                    criteriaBuilder.like(products.get(PRODUCT_NAME_FIELD), like(stringForSearch))));
         });
 
         orderFilter.getStatusOptional().ifPresent(status ->
                 wherePredicates.add(criteriaBuilder.equal(root.get(STATUS_FIELD), status)));
 
         stringForSearchOptional.ifPresent(stringForSearch ->
-                wherePredicates.add(criteriaBuilder.like(root.get(COMMENT_FIELD), like(stringForSearch))));
+            stringForSearchPredicates.add(criteriaBuilder.like(root.get(COMMENT_FIELD), like(stringForSearch))));
 
         orderFilter.getToPriceOptional().ifPresent(toPrice ->
                 wherePredicates.add(criteriaBuilder.lessThanOrEqualTo(
@@ -104,6 +110,8 @@ public class ExtendedOrderRepository {
                 wherePredicates.add(criteriaBuilder.lessThanOrEqualTo(
                         root.get(CREATED_DATE_FIELD), toDate)));
 
+        Predicate stringForSearchPredicate = criteriaBuilder.or(stringForSearchPredicates.toArray(new Predicate[0]));
+        wherePredicates.add(stringForSearchPredicate);
         Predicate[] wherePredicatesArray = wherePredicates.toArray(new Predicate[0]);
 
         query.where(wherePredicatesArray);
