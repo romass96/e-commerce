fae63a519113293e9098849d70e5930a31f5fb95
Roman Babii <romababiy96@gmail.com>
2020-11-27 19:42:42 +0200

    extended ProductFilter with fromPrice and toPrice fields

--- src/main/java/ua/ugolek/payload/filters/ProductFilter.java
+++ src/main/java/ua/ugolek/payload/filters/ProductFilter.java
@@ -3,9 +3,21 @@ package ua.ugolek.payload.filters;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Optional;
+
 @Getter
 @Setter
 public class ProductFilter extends SearchFilter
 {
     private boolean archived;
+    private Double fromPrice;
+    private Double toPrice;
+
+    public Optional<Double> getFromPriceOptional() {
+        return Optional.ofNullable(fromPrice);
+    }
+
+    public Optional<Double> getToPriceOptional() {
+        return Optional.ofNullable(toPrice);
+    }
 }
--- src/main/java/ua/ugolek/repository/dto/extractors/ProductDTOExtractor.java
+++ src/main/java/ua/ugolek/repository/dto/extractors/ProductDTOExtractor.java
@@ -3,7 +3,6 @@ package ua.ugolek.repository.dto.extractors;
 import ua.ugolek.dto.ProductDTO;
 import ua.ugolek.model.Product;
 import ua.ugolek.payload.filters.ProductFilter;
-import ua.ugolek.payload.filters.SearchFilter;
 
 import javax.persistence.EntityManager;
 import javax.persistence.criteria.CriteriaQuery;
@@ -19,6 +18,7 @@ public class ProductDTOExtractor extends DTOExtractor<Product, ProductFilter, Pr
 
     private static final String ARCHIVING_DETAILS_FIELD = "archivingDetails";
     private static final String ARCHIVED_FIELD = "archived";
+    private static final String PRICE_FIELD = "price";
 
     public ProductDTOExtractor(ProductFilter filter, EntityManager entityManager,
                                Function<Product, ProductDTO> dtoMapper) {
@@ -36,6 +36,14 @@ public class ProductDTOExtractor extends DTOExtractor<Product, ProductFilter, Pr
             Predicate stringForSearchPredicate = criteriaBuilder.or(predicates);
             wherePredicates.add(stringForSearchPredicate);
         });
+
+        filter.getToPriceOptional().ifPresent(toPrice ->
+            wherePredicates.add(criteriaBuilder.lessThanOrEqualTo(
+                root.get(PRICE_FIELD), toPrice)));
+
+        filter.getFromPriceOptional().ifPresent(fromPrice ->
+            wherePredicates.add(criteriaBuilder.greaterThanOrEqualTo(
+                root.get(PRICE_FIELD), fromPrice)));
 
         wherePredicates.add(criteriaBuilder.equal(
             root.get(ARCHIVING_DETAILS_FIELD).get(ARCHIVED_FIELD), filter.isArchived()));
